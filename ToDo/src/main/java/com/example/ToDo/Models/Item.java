bef7dc311ac19e1bf4952a868fa4bdae6fb7d1ba
ddang9390 <dnld1ng@yahoo.com>
2019-05-26 14:20:31 -0400

    Forgot to include web dependency and made item model class

---
+++ ToDo/src/main/java/com/example/ToDo/Models/Item.java
@@ -0,0 +1,45 @@
+package com.example.ToDo.Models;
+
+import org.apache.tomcat.jni.Time;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "items")
+public class Item {
+    @Id
+    String id;
+    String name;
+    String desc;
+    Time time;
+
+    public Item(String name, String desc, Time time){
+        this.name = name;
+        this.desc = desc;
+        this.time = time;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDesc(){
+        return desc;
+    }
+
+    public void setDesc(String desc){
+        this.desc = desc;
+    }
+
+    public Time getTime(){
+        return time;
+    }
+
+    public void setTime(Time time){
+        this.time = time;
+    }
+}
--- ToDo/src/main/java/com/example/ToDo/ToDoApplication.java
+++ ToDo/src/main/java/com/example/ToDo/ToDoApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.ToDo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodoApplication {
+public class ToDoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TodoApplication.class, args);
+		SpringApplication.run(ToDoApplication.class, args);
 	}
 
 }
